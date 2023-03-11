6f988026f7e2455014aaff63a04aa83339cc7db6
adrinfer <adrinfer@gmail.com>
2023-03-11 19:52:31 +0100

    A

--- src/components/fixed/FooterBar.jsx
+++ src/components/fixed/FooterBar.jsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import Toolbar from '@mui/material/Toolbar'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import SearchIcon from '@mui/icons-material/Search'
-import Typography from '@mui/material/Typography'
-import Link from '@mui/material/Link'
-import { AppBar, Box } from '@mui/material'
+import { AppBar, Box, TextField } from '@mui/material'
 import Fab from '@mui/material/Fab'
 import AddIcon from '@mui/icons-material/Add'
-import logo from '../../assets/pixil-frame-0.png'
+import Button from '@mui/material/Button'
+import Fade from '@mui/material/Fade'
+import Paper from '@mui/material/Paper'
+import Popper from '@mui/material/Popper'
+import Typography from '@mui/material/Typography'
+import ClickAwayListener from '@mui/base/ClickAwayListener';
 
 const StyledFab = styled(Fab)({
   position: 'absolute',
@@ -21,11 +21,36 @@ const StyledFab = styled(Fab)({
 })
 
 function FooterBar (props) {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const [open, setOpen] = React.useState(false)
+
+  const handleAddClick = () => e => {
+    setOpen(true)
+    setAnchorEl(e.currentTarget)
+  }
+
   return (
     <React.Fragment>
+      <Popper open={open} anchorEl={anchorEl} placement='top' transition >
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper style={{ marginBotton: '200px' }}>
+              <TextField
+                id="outlined-multiline-flexible"
+                label="Multiline"
+                multiline
+                maxRows={4}
+                style={{ minWidth: '300px', maxWidth: '400px' }}
+              />
+
+            </Paper>
+
+          </Fade>
+        )}
+      </Popper>
       <AppBar sx={{ top: 'auto', bottom: 0, height: 40 }}>
         <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <StyledFab color="secondary" aria-label="add">
+          <StyledFab onClick={handleAddClick()} color="secondary" aria-label="add">
             <AddIcon />
           </StyledFab>
         </Toolbar>
