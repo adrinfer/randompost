23b11ebffb03eb815d72bab4db320e70cba0422e
adrinfer <adrinfer@gmail.com>
2023-03-11 18:31:48 +0100

    AASDSA

--- backend/MongoClient2.js
+++ backend/MongoClient2.js
@@ -4,13 +4,7 @@ const uri = 'mongodb+srv://adrinfer:zYrAeL6640423181962@randompost00.i0agwcr.mon
 const mongo = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
  */
 
-import { MongoClient, ServerApiVersion } from 'mongodb'
-const uri = 'mongodb+srv://adrinfer:zYrAeL6640423181962@randompost00.i0agwcr.mongodb.net/?retryWrites=true&w=majority'
-const mongo = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-console.log('jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')
-
-export const mongoDB = mongo.connect()
-// import { mongoDB } from '../utils/MongoClient'
+import { mongoDB } from '../utils/MongoClient'
 
 const handler = async (event) => {
   (await mongoDB).db('RandomPost').collection('AAAAAA').insertOne({ probando: 'BBBBBB' })
